ffed884d974c7b72888de03d162f9b79bf702e7b
marcosaron <141440615+marcosaron@users.noreply.github.com>
2023-09-03 17:41:45 -0300

    Sitio web creado

---
+++ events.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const div = document.querySelector('.Button');
+
+    div.addEventListener('click', function() {
+        alert('Hola! Soy el div');
+    });
+});
+
+function mostrarAlerta() {
+    alert("¡Hola!");
+    }
+    
